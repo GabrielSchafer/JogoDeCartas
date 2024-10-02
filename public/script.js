d05fe410234e0bd6b96f2ce313db6d07b5a04336
Gabriel Heyde <128318812+GabrielHeyde@users.noreply.github.com>
2024-10-01 21:42:57 -0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,88 @@
+const socket = io(); // Conecta ao servidor
+
+let salaAtual;
+let numeroCartaMonte = 0;
+let userName;
+
+function verificaRodada(){
+    //se for a sua vez de jogar habilita o botão
+    //Teremos um array de 4 rotacoes, na primeira rodada o jogador da posicao 0 no array irá jogar, após ele jogar a carta vai ter uma iteracao no array, após a ultima posicao no array ele irá resetar a iterador e assim segue a rodada. essa function vai retornar ao jogador se é a vez dele com um boolean, cada jogador vai ter o numero da sua posicao e toda rotacao vai comparar a posicao com o numero no array, esse array vai receber os jogadores para poder desabilitar o botao do usuario correto.
+}
+
+function habilitaJogarCarta(){
+    // Precisamos da classe usuario com os atributos corretos
+    if(verificaRodada()){
+        //usuario.disable()=false;
+    }
+    else{
+        //usuario.disable()=true;
+    }
+
+
+}
+
+// Oatualiza o número de jogadores
+socket.on('atualizaJogadores', (data) => {
+    const salaDiv = document.getElementById("numeroJogadores"); //
+    salaDiv.textContent = "Número de jogadores: " +`${data.numeroJogadores}/${data.maxJogadores}`;
+});
+
+//  mensagem de entrada e saída
+socket.on('mensagem', (msg) => {
+    console.log(msg); 
+    alert(msg); 
+});
+
+// Função para entrar em uma sala
+function entrarSala(sala) {
+
+    userName = prompt("Por favor, insira seu nome:");
+    
+    if (!userName) {
+        alert("Você precisa inserir um nome para entrar na sala.");
+        return; // Se o nome não for fornecido, sai da função
+    }
+
+        salaAtual = sala; // Salva a sala atual
+        socket.emit('entrarSala', {sala, userName}); // Envia o evento ao servidor
+
+        // Muda da tela inicial para a tela do jogo
+        document.getElementById("telaInicial").classList.add("hidden");
+        //document.getElementById("fila").classList.remove("hidden");
+        document.getElementById("telaJogo").classList.remove("hidden");
+
+        // Atualiza a tela com o nome do usuário e sala
+        document.getElementById("nomeUsuario").innerText = `Usuário: ${userName}`;
+        document.getElementById("salaAtual").innerText = `Sala: ${sala}`;
+
+        console.log(`Entrou na ${sala} como ${userName}`);
+}
+
+// Função ao clicar no botão "Bater"
+function bater() {
+    console.log("Bateu na mesa!");
+    socket.emit('bater', { usuarioNome, sala: salaAtual }); 
+}
+
+function escolherCarta(){
+    console.log("Jogou a carta");
+    const elementoMonte = document.getElementById("numeroMonte")
+    numeroCartaMonte++;
+    elementoMonte.textContent= "Monte: " + numeroCartaMonte;
+    socket.emit('jogar', { usuarioNome, sala: salaAtual });
+}
+// Listener para a resposta do servidor sobre quem bateu
+socket.on('bateu', (data) => {
+    if (data.jogador === 'Você') {
+        alert('Você bateu na mesa!');
+    } else {
+        alert(`${data.jogador} bateu na mesa!`);
+    }
+});
+socket.on('jogar', (data) => {
+    if (data.jogador === 'Você') {
+        alert('Você jogou a carta!');
+    } else {
+        alert(`${data.jogador} jogou a carta!`);
+    }
+});
---
+++ server.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const salas = {
+    'Sala 1': { numero: 0, max: 4 },
+
+};
+
+let monteCartas = {};
+
+function Carta(numeroCarta, imagemCarta){
+    this.numeroCarta = numeroCarta;
+    this.imagemCarta = imagemCarta;
+}
+
+
+//criando o baralho
+const baralho = [];
+function criaBaralho(baralho){
+    for(let i = 0; i < 8;i++){
+        for(let j = 0; j < 13;j++){
+            let carta = new Carta(j,`${j}.png` );
+            baralho.push(carta);
+        }
+    }
+    //ao criar o baralho na mesma funçao eu irei embaralhar as cartas
+    for(let i = 0; i < 10;i++){
+        baralho.sort(() => Math.random() - 0.5);
+    }
+
+}
+
+function distribuirCarta(i){
+
+}
+
+function User(userName, id, baralho){
+    this.id = id;
+    this.name = userName;
+    this.baralho = baralho;
+}
+
+let salaCheia;
+const usuarios = {}; 
+
+app.use(express.static('public'));
+
+//criando o baralho
+criaBaralho(baralho);
+console.log(baralho.length)
+
+
+io.on('connection', (socket) => {
+    console.log('Usuário conectado:', socket.id);
+
+    socket.on('entrarSala', ({ sala, userName }) => {
+        socket.join(sala);
+        salas[sala].numero += 1;
+
+        // Armazena o usuário no objeto `usuarios`
+        usuarios[socket.id] = { nome: userName, sala};
+
+        // Notifica os outros usuários na sala
+        socket.to(sala).emit('mensagem', `${userName} entrou na sala!`);
+        
+        // Atualiza todos os usuários sobre o número de jogadores
+        io.to(sala).emit('atualizaJogadores', { sala, numeroJogadores: salas[sala].numero, maxJogadores: salas[sala].max });
+    });
+
+    socket.on('bater', (data) => {
+        console.log(`${data.usuarioNome} bateu!`); 
+
+        socket.emit('bateu', { jogador: 'Você' });
+
+        socket.to(data.sala).emit('bateu', { jogador: data.usuarioNome });
+    });
+
+    socket.on('jogar', (data) => {
+        console.log(`${data.usuarioNome} jogou a carta!`);
+
+        socket.emit('jogar', { jogador: 'Você' });
+
+        socket.to(data.sala).emit('jogar', { jogador: data.usuarioNome });
+    });
+
+    socket.on('disconnect', () => {
+        const usuario = usuarios[socket.id];
+        if (usuario) {
+            const { nome, sala } = usuario; // Desestrutura o nome e a sala
+            salas[sala].numero -= 1; // Reduz o número de jogadores na sala
+            console.log(`Usuário desconectado: ${nome}`);
+            
+            // Notifica os outros usuários que um usuário saiu
+            socket.to(sala).emit('mensagem', `${nome} saiu da sala.`);
+            
+            // Atualiza todos os usuários sobre o número de jogadores
+            io.to(sala).emit('atualizaJogadores', { sala, numeroJogadores: salas[sala].numero, maxJogadores: salas[sala].max });
+            
+            // Remove o usuário do objeto `usuarios`
+            delete usuarios[socket.id]; 
+        }
+        console.log('Usuário desconectado:', socket.id);
+    });
+
+    socket.on(`salaCheia`, () =>{
+        if(salas[sala].numero >= salas[sala].max){
+
+        }
+    })
+
+});
+
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
